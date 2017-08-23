48cf24856ac5650a7f53f4672dc32f56775a9f22
Roger Taracha <rojtaracha@gmail.com>
2017-08-23 16:24:00 +0300

    Unit Test 1: Opening the `Add Notes` screen: [+] Our first test verifies that asking the Presenter to `Add a new note` results in a request to be made for the View to show the new add-note screen.

--- app/src/test/java/com/example/android/testing/notes/notes/NotesPresenterTest.java
+++ app/src/test/java/com/example/android/testing/notes/notes/NotesPresenterTest.java
@@ -90,12 +90,11 @@ public class NotesPresenterTest {
 
     @Test
     public void clickOnFab_ShowsAddsNoteUi() {
-        fail("Implement in step 6");
-//        // When adding a new note
-//        mNotesPresenter.addNewNote();
-//
-//        // Then add note UI is shown
-//        verify(mNotesView).showAddNote();
+        // when adding a new note
+        mNotesPresenter.addNewNote();
+
+        // Then add note UI is shown
+        verify(mNotesView).showAddNote();
     }
 
     @Test
